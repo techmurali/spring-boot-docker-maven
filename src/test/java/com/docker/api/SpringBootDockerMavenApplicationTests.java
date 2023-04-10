85cd3fe3f318e035500600b89a709a323960d28a
techmurali <muralikrishna.paidi@gmail.com>
2023-04-10 16:53:09 +0530

    first commit

---
+++ src/test/java/com/docker/api/SpringBootDockerMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docker.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDockerMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
